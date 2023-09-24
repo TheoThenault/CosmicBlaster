f60892f20f73fbcd2f29eabf189352ed24106e96
TheoThenault <theo.thenault@gmail.com>
2023-09-24 20:57:56 +0200

    Apparition des asteroides

---
+++ Assets/Game/Scripts/SpawnAsteroids.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAsteroids : MonoBehaviour
+{
+    public float SpawnRate = 2f;
+
+    public int AsteroidCount = 0;
+
+    public int MaxAsteroidCount = 10;
+
+    public GameObject AsteroidPrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnAsteroid", 0f, SpawnRate);
+    }
+
+    void SpawnAsteroid()
+    {
+        if(AsteroidCount < MaxAsteroidCount)
+        {
+            if(AsteroidPrefab != null)
+            {
+                GameObject newAsteroid = Instantiate(AsteroidPrefab);
+                newAsteroid.SetActive(true);
+                AsteroidCount++;
+            }
+        }
+    }
+}
