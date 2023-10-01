cb28777774b9130a004ef0c17d3984ab19ca194d
TheoThenault <theo.thenault@gmail.com>
2023-10-01 21:00:27 +0200

    Les balles tirent droit mais suivent encore le vaisseaux

---
+++ Assets/Game/Scripts/BulletBehavior.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public float BulletVelocity = 10f;
+
+    public float MaxDistance = 1000f;
+
+    private Transform m_transform = null;
+
+    public GameObject Spaceship = null;
+
+    private Vector3 movementDirection = new Vector3(0f, -1f, 0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_transform = GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(m_transform != null) 
+        { 
+            m_transform.Translate(movementDirection * BulletVelocity);
+
+            if(Spaceship != null)
+            {
+                Transform spaceshipTransform = Spaceship.transform;
+                if (spaceshipTransform != null)
+                {
+                    float distance = (spaceshipTransform.position - m_transform.position).magnitude;
+
+                    if (distance > MaxDistance)
+                    {
+                        Destroy(this.gameObject);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Game/Scripts/SpaceshipFire.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipFire : MonoBehaviour
+{
+    public GameObject Bullet;
+
+    public float FiringDelay = 0.5f;
+
+    private float lastFire = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float fire = Input.GetAxis("Fire1");
+
+        if( fire > 0.9 ) 
+        {
+            float delta = Time.time - lastFire;
+            if(delta > FiringDelay )
+            {
+                if (Bullet != null)
+                {
+                    GameObject newBullet = Instantiate(Bullet, this.transform, true);
+                    if (newBullet != null)
+                    {
+                        newBullet.SetActive(true);
+                        Transform transform = newBullet.GetComponent<Transform>();
+                    }
+                }
+                lastFire = Time.time;
+            }
+        }
+    }
+}
