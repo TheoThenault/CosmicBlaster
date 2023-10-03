4b7dcad5d3aedc3845b56e6e0a5343285c79adac
TheoThenault <theo.thenault@gmail.com>
2023-10-03 13:13:44 +0200

    Ajout de l'éxplosion

--- Assets/Game/Scripts/SpaceshipCollision.cs
+++ Assets/Game/Scripts/SpaceshipCollision.cs
@@ -8,6 +8,10 @@ public class SpaceshipCollision : MonoBehaviour
 
     private PlayerStatistics playerStatistics = null;
 
+    public GameObject Explosion = null;
+
+    private bool m_spaceshipDestroyed = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +21,7 @@ public class SpaceshipCollision : MonoBehaviour
 
     public void HandleCollisions(Collision collision)
     {
-        if (collision != null && collision.gameObject != null && collision.gameObject.tag == "Asteroid")
+        if (!m_spaceshipDestroyed && collision != null && collision.gameObject != null && collision.gameObject.tag == "Asteroid")
         {
 
             if (AsteroidsManager != null)
@@ -29,7 +33,14 @@ public class SpaceshipCollision : MonoBehaviour
                     playerStatistics.removeHealth();
 
                     if (playerStatistics.IsDead())
+                    {
+                        m_spaceshipDestroyed = true;
+
+                        if(Explosion != null)
+                            Instantiate(Explosion, transform.position, transform.rotation);
+
                         Destroy(this.gameObject);
+                    }
                 }
             }
         }
---
+++ Assets/ParticleProFX/Resources/Scripts/PPFXPhysicForce.cs
@@ -0,0 +1,50 @@
+﻿/*=========================================================
+	PARTICLE PRO FX volume one 
+	PPFXPhysicForce.cs
+	
+	Add Rigidbody force to explosions.
+	
+	(c) 2014
+=========================================================*/
+
+using UnityEngine;
+using System.Collections;
+
+public class PPFXPhysicForce : MonoBehaviour {
+	
+	public float radius = 10f;
+	public float force = 10f;
+	public float delay = 0.2f;
+	
+	Collider[] colliders;
+	
+	
+	void Start () 
+	{
+		colliders = Physics.OverlapSphere(this.transform.position, radius);
+		
+		StartCoroutine(Explode());
+	}
+	
+	IEnumerator Explode()
+	{
+		yield return new WaitForSeconds(delay);
+		
+		for(int i = 0; i < colliders.Length; i ++)
+		{
+            // Par défaut 
+            /*
+			var _rb = colliders[i].GetComponent<Rigidbody>();
+            if (_rb != null)
+            {
+                colliders[i].GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, radius, new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), Random.Range(-3, 3)), ForceMode.Impulse);
+            }
+			*/
+
+            // Modifier pour ignorer la physique des explosions
+        }
+		
+		yield return null;
+	}
+
+}
